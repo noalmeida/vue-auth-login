2e7c3fb7f48c5cdc5839acc532d16d32216b3bca
Noan <noamwt@gmail.com>
2021-09-13 20:53:33 -0300

    server express

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+
 var app = express()
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 app.listen(port)
+console.log('server started ' + port)
